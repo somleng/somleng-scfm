d0e2daf97b427617f7d4934175c7fca8c62044c9
David Wilkie <dwilkie@gmail.com>
2017-11-15 15:03:44 +0700

    Specify foreign_key arguments correctly in migrations

--- db/migrate/20170926070448_create_callout_participations.rb
+++ db/migrate/20170926070448_create_callout_participations.rb
@@ -3,7 +3,13 @@ class CreateCalloutParticipations < ApplicationMigration
     create_table :callout_participations do |t|
       t.references(:callout, :foreign_key => true, :index => true, :null => false)
       t.references(:contact, :foreign_key => true, :index => true, :null => false)
-      t.references(:callout_population, :foreign_key => true, :index => true)
+      t.references(
+        :callout_population,
+        :index => true,
+        :foreign_key => {
+          :to_table => :batch_operations
+        }
+      )
       t.string(:msisdn, :null => false)
       t.public_send(json_column_type, :metadata, :null => false, :default => json_column_default)
       t.index([:callout_id, :msisdn], :unique => true)
--- db/migrate/20170926070754_create_phone_calls.rb
+++ db/migrate/20170926070754_create_phone_calls.rb
@@ -3,9 +3,27 @@ class CreatePhoneCalls < ApplicationMigration
     create_table :phone_calls do |t|
       t.references(:callout_participation, :index => true, :foreign_key => true)
       t.references(:contact, :null => false, :index => true, :foreign_key => true)
-      t.references(:create_batch_operation, :index => true, :foreign_key => true)
-      t.references(:queue_batch_operation, :index => true, :foreign_key => true)
-      t.references(:queue_remote_fetch_batch_operation, :index => true, :foreign_key => true)
+      t.references(
+        :create_batch_operation,
+        :index => true,
+        :foreign_key => {
+          :to_table => :batch_operations
+        }
+      )
+      t.references(
+        :queue_batch_operation,
+        :index => true,
+        :foreign_key => {
+          :to_table => :batch_operations
+        }
+      )
+      t.references(
+        :queue_remote_fetch_batch_operation,
+        :index => true,
+        :foreign_key => {
+          :to_table => :batch_operations
+        }
+      )
       t.string(:status, :null => false)
       t.string(:msisdn, :null => false)
       t.string(:remote_call_id, :index => {:unique => true})

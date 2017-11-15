5dd89389ae607a65b27239323898cc42e51fec84
David Wilkie <dwilkie@gmail.com>
2017-11-15 20:05:48 +0700

    Use DATE function of sqlite when comparing dates

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,27 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+  def self.timestamp_attribute_is(comparator, value, timestamp_attribute)
+    if value.is_a?(String)
+      query_value = DateTime.parse(value)
+      query_value = query_value.to_date if query_value == Date.parse(value)
+    else
+      query_value = value
+    end
+
+    arel_timestamp_attribute = query_value.class == Date ? cast_as_date(timestamp_attribute) : arel_table[timestamp_attribute]
+
+    where(arel_timestamp_attribute.public_send(comparator, query_value))
+  end
+
+  def self.cast_as_date(attribute)
+    if database_adapter_helper.adapter_sqlite?
+      Arel::Nodes::NamedFunction.new('DATE', [arel_table[attribute]])
+    else
+      Arel::Nodes::NamedFunction.new('CAST', [arel_table[attribute].as('DATE')])
+    end
+  end
+
   def self.database_adapter_helper
     @database_adapter_helper ||= DatabaseAdapterHelper.new
   end
--- app/models/filter/attribute/timestamp.rb
+++ app/models/filter/attribute/timestamp.rb
@@ -8,10 +8,8 @@ class Filter::Attribute::Timestamp < Filter::Attribute::Base
 
   def apply
     scope = association_chain.all
-    valid_query_mappings.each do |query_mapping, query_values|
-      raw_query_value, datetime_query_value = query_values
-      arel_datetime_attribute = date_given?(raw_query_value) ? cast_as_date : association_chain.arel_table[timestamp_attribute]
-      scope = scope.where(arel_datetime_attribute.public_send(query_mapping, datetime_query_value))
+    valid_query_mappings.each do |query_mapping, filter_value|
+      scope = scope.timestamp_attribute_is(query_mapping, filter_value, timestamp_attribute)
     end
     scope
   end
@@ -30,14 +28,9 @@ class Filter::Attribute::Timestamp < Filter::Attribute::Base
     Hash[
       filter_query_mappings.map do |k, v|
         filter_value = params[filter_param_key(k)]
-        timestamp_filter_value = parse_filter_value(filter_value)
-        [v, timestamp_filter_value && [filter_value, timestamp_filter_value]]
+        [v, parse_filter_value(filter_value) && filter_value]
       end
     ].compact
-  end
-
-  def cast_as_date
-    Arel::Nodes::NamedFunction.new('CAST', [association_chain.arel_table[timestamp_attribute].as('DATE')])
   end
 
   def filter_query_mappings
@@ -51,10 +44,6 @@ class Filter::Attribute::Timestamp < Filter::Attribute::Base
 
   def filter_param_key(key)
     [timestamp_attribute, key].join("_").to_sym
-  end
-
-  def date_given?(raw_value)
-    DateTime.parse(raw_value) == Date.parse(raw_value)
   end
 
   def parse_filter_value(raw_value)

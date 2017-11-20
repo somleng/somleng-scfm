05a9438f745992c4f892bef9e8410d24f2d2cc2c
David Wilkie <dwilkie@gmail.com>
2017-11-20 16:28:34 +0700

    Constantize call flow logic in initializer to ensure it's loaded. #67

--- config/initializers/call_flow_logic.rb
+++ config/initializers/call_flow_logic.rb
@@ -1,3 +1,4 @@
-if registered_call_flow_logic = ENV["REGISTERED_CALL_FLOW_LOGIC"]
-  CallFlowLogic::Base.register(*registered_call_flow_logic.to_s.split(","))
+if names = ENV["REGISTERED_CALL_FLOW_LOGIC"]
+  class_names = names.to_s.split(",").map { |name| klass = name.constantize rescue nil; klass && klass.to_s }.compact
+  CallFlowLogic::Base.register(*class_names)
 end

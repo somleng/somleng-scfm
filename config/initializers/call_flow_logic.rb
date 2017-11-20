9d9098cdd17c14f2754d6234d6b624dd868a2d29
David Wilkie <dwilkie@gmail.com>
2017-11-20 14:23:11 +0700

    Fix syntax error when registering call flow logic. #67

--- config/initializers/call_flow_logic.rb
+++ config/initializers/call_flow_logic.rb
@@ -1,5 +1,5 @@
 if registered_call_flow_logic = ENV["REGISTERED_CALL_FLOW_LOGIC"]
-  permitted_call_flow_logic = CallFlowLogic::Base.descendents.map(&:to_s)
+  permitted_call_flow_logic = CallFlowLogic::Base.descendants.map(&:to_s)
 
   registered_call_flow_logic.to_s.split(",").each do |call_flow_logic|
     CallFlowLogic::Base.register(call_flow_logic) if permitted_call_flow_logic.include?(call_flow_logic)

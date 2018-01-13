52517f58e0b7ce26824475da1fd32e67e397ac07
David Wilkie <dwilkie@gmail.com>
2018-01-13 10:45:24 +0700

    Add application seeder to create super admin accounts. #79

--- db/application_seeder.rb
+++ db/application_seeder.rb
@@ -1,12 +1,35 @@
 class ApplicationSeeder
+  OUTPUTS = {
+    :all => "all",
+    :none => "none",
+    :super_admin => "super_admin",
+  }
+
+  DEFAULT_OUTPUT = :none
+
   def initialize(options = {})
   end
 
   def seed!
-    create_super_admin_account!
+    if create_super_admin_account?
+      super_admin_account = create_super_admin_account!
+      print_account_info(super_admin_account, "Super Admin") if output_super_admin?
+    end
   end
 
   private
+
+  def create_super_admin_account?
+    ENV["CREATE_SUPER_ADMIN_ACCOUNT"].to_i == 1
+  end
+
+  def output
+    @output ||= OUTPUTS[default_output]
+  end
+
+  def default_output
+    (ENV["OUTPUT"] && ENV["OUTPUT"].to_sym) || DEFAULT_OUTPUT
+  end
 
   def create_super_admin_account!
     permissions = [:super_admin]
@@ -18,7 +41,23 @@ class ApplicationSeeder
       account.save!
     end
 
+    account.access_tokens.first_or_create!(:created_by => account)
+
     account
+  end
+
+  def output_all?
+    output == OUTPUTS[:all]
+  end
+
+  def output_super_admin?
+    output == OUTPUTS[:super_admin] || output_all?
+  end
+
+  def print_account_info(account, type)
+    print(
+      "#{type} Account Access Token: #{account.access_tokens.first.token}\n"
+    )
   end
 end
 
---
+++ spec/db/application_seeder_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+require Rails.root.join('db/application_seeder')
+
+RSpec.describe ApplicationSeeder do
+  let(:initialization_options) { {} }
+  subject { described_class.new(initialization_options) }
+
+  describe "#seed!" do
+    let(:asserted_outputs) { [] }
+
+    def env
+      {}
+    end
+
+    def seed!
+      subject.seed!
+    end
+
+    def assert_seed!
+      assert_outputs!(asserted_outputs) { seed! }
+    end
+
+    def assert_outputs!(asserted_outputs, &block)
+      expectation = expect { yield }
+
+      if asserted_outputs.empty?
+        expectation.not_to output.to_stdout
+      else
+        expectation.to output(
+          Regexp.new(asserted_outputs.join(".+"), Regexp::MULTILINE)
+        ).to_stdout
+      end
+    end
+
+    context "by default" do
+      def assert_seed!
+        super
+        expect(Account.count).to eq(0)
+      end
+
+      it { assert_seed! }
+    end
+
+    context "CREATE_SUPER_ADMIN_ACCOUNT=1" do
+
+
+      def env
+        super.merge("CREATE_SUPER_ADMIN_ACCOUNT" => "1")
+      end
+
+      def assert_seed!
+        super
+        expect(Account.with_permissions(:super_admin).count).to eq(1)
+        expect(Account.count).to eq(1)
+        created_account = Account.first!
+        expect(created_account.access_tokens.count).to eq(1)
+      end
+
+      context "by default" do
+        it { assert_seed! }
+      end
+
+      context "OUTPUT=super_admin" do
+        let(:asserted_outputs) { ["Super Admin Account Access Token"] }
+
+        def env
+          super.merge("OUTPUT" => "super_admin")
+        end
+
+        it { assert_seed! }
+      end
+    end
+  end
+end
+

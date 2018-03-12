15af1664c18d9cc2c3af0155cf4e2b6da34f9304
simleap <simleap@gmail.com>
2018-03-12 16:14:00 +0700

    disable user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+    get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', as: 'user_registration'
+  end
+
+  devise_for :users, skip: :registrations
+
   root to: redirect('dashboard')
 
   namespace 'dashboard' do

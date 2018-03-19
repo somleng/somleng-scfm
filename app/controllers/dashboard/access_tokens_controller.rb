4b98ce5a629e417f16f9d8e6010b39faa73cdb55
simleap <simleap@gmail.com>
2018-03-19 20:39:42 +0700

    add kaminari pagination view

--- app/controllers/dashboard/access_tokens_controller.rb
+++ app/controllers/dashboard/access_tokens_controller.rb
@@ -1,5 +1,5 @@
 class Dashboard::AccessTokensController < Dashboard::BaseController
   def index
-    @access_tokens = current_account.access_tokens
+    @access_tokens = current_account.access_tokens.page(params[:page]).per(10)
   end
 end

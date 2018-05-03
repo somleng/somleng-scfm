5eaf75423e0e71af5b7dfe1342728ff2df1afd6c
David Wilkie <dwilkie@gmail.com>
2018-05-03 18:30:11 +0700

    Fix capitalization for callouts and contacts

--- spec/system/dashboard/callouts_spec.rb
+++ spec/system/dashboard/callouts_spec.rb
@@ -1,10 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Callout pages', type: :system do
+RSpec.describe "Callout pages", type: :system do
   let(:user) { create(:user) }
 
-  describe 'view all callout' do
-    it 'should list all callouts of current account' do
+  describe "view all callout" do
+    it "should list all callouts of current account" do
       callout = create(:callout, account: user.account)
       other_callout = create(:callout)
 
@@ -15,19 +15,19 @@ RSpec.describe 'Callout pages', type: :system do
       expect(page).not_to has_callout(other_callout)
     end
 
-    it 'can create new callout' do
+    it "can create a callout" do
       sign_in(user)
       visit dashboard_callouts_path
 
-      click_button 'New callout'
-      click_button 'Create Callout'
+      click_button "New Callout"
+      click_button "Create Callout"
 
-      expect(page).to have_text('Callout was successfully created.')
+      expect(page).to have_text("Callout was successfully created.")
     end
   end
 
-  describe 'callout detail' do
-    it 'click edit will open edit page' do
+  describe "callout detail" do
+    it "click edit will open edit page" do
       callout = create(:callout, account: user.account)
 
       sign_in(user)
@@ -35,20 +35,20 @@ RSpec.describe 'Callout pages', type: :system do
 
       expect(page).to has_callout(callout)
 
-      click_button 'Edit'
-      click_button 'Update Callout'
+      click_button "Edit"
+      click_button "Update Callout"
 
-      expect(page).to have_text('Callout was successfully updated.')
+      expect(page).to have_text("Callout was successfully updated.")
     end
 
-    it 'click delete callout then accept alert' do
+    it "click delete callout then accept alert" do
       callout = create(:callout, account: user.account)
 
       sign_in(user)
       visit dashboard_callout_path(callout)
-      click_button 'Delete'
+      click_button "Delete"
 
-      expect(page).to have_text('Callout was successfully destroyed.')
+      expect(page).to have_text("Callout was successfully destroyed.")
     end
   end
 
--- spec/system/dashboard/contacts_spec.rb
+++ spec/system/dashboard/contacts_spec.rb
@@ -1,14 +1,14 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Contact pages', type: :system do
+RSpec.describe "Contact pages", type: :system do
   let(:user) { create(:user) }
 
   before do
     sign_in(user)
   end
 
-  describe 'view all contacts' do
-    it 'should list all contacts of current account' do
+  describe "view all contacts" do
+    it "should list all contacts of current account" do
       account_contact = create(:contact, account: user.account)
       other_contact = create(:contact)
 
@@ -18,83 +18,83 @@ RSpec.describe 'Contact pages', type: :system do
       expect(page).not_to have_text(other_contact.msisdn)
     end
 
-    it 'on click new contact will open new contact page' do
-      visit 'dashboard/contacts'
+    it "on click new contact will open new contact page" do
+      visit "dashboard/contacts"
 
-      click_button 'New contact'
+      click_button "New Contact"
       expect(page).to have_current_path(new_dashboard_contact_path)
     end
   end
 
-  describe 'new contact' do
-    it 'successfully create new contact' do
+  describe "new contact" do
+    it "successfully create new contact" do
       visit new_dashboard_contact_path
 
-      fill_in 'contact[msisdn]', with: generate(:somali_msisdn)
-      click_button 'Create Contact'
+      fill_in "contact[msisdn]", with: generate(:somali_msisdn)
+      click_button "Create Contact"
 
-      expect(page).to have_text('Contact was successfully created.')
+      expect(page).to have_text("Contact was successfully created.")
     end
 
-    it 'on valid, will render the same page with error message' do
+    it "on valid, will render the same page with error message" do
       visit new_dashboard_contact_path
 
-      fill_in 'contact[msisdn]', with: ''
-      click_button 'Create Contact'
+      fill_in "contact[msisdn]", with: ""
+      click_button "Create Contact"
 
-      expect(page).to have_text('New contact')
+      expect(page).to have_text("New Contact")
       expect(page).to have_text("can't be blank")
     end
   end
 
-  describe 'contact detail' do
-    it 'show contact detail' do
+  describe "contact detail" do
+    it "show contact detail" do
       contact = create(:contact, account: user.account)
 
       visit dashboard_contact_path(contact)
 
-      expect(page).to have_text('Contact detail')
+      expect(page).to have_text("Contact detail")
     end
 
-    it 'click edit will open edit page' do
+    it "click edit will open edit page" do
       contact = create(:contact, account: user.account)
 
       visit dashboard_contact_path(contact)
-      click_button 'Edit'
+      click_button "Edit"
 
       expect(page).to have_current_path(edit_dashboard_contact_path(contact))
     end
 
-    it 'click delete contact then accept alert' do
+    it "click delete contact then accept alert" do
       contact = create(:contact, account: user.account)
 
       visit dashboard_contact_path(contact)
-      click_button 'Delete'
-      expect(page).to have_text('Contact was successfully destroyed.')
+      click_button "Delete"
+      expect(page).to have_text("Contact was successfully destroyed.")
     end
   end
 
-  describe 'edit contact' do
-    it 'successfully edit contact' do
+  describe "edit contact" do
+    it "successfully edit contact" do
       contact = create(:contact, account: user.account)
 
       visit edit_dashboard_contact_path(contact)
 
-      fill_in 'contact[msisdn]', with: generate(:somali_msisdn)
-      click_button 'Update Contact'
+      fill_in "contact[msisdn]", with: generate(:somali_msisdn)
+      click_button "Update Contact"
 
-      expect(page).to have_text('Contact was successfully updated.')
+      expect(page).to have_text("Contact was successfully updated.")
     end
 
-    it 'on valid, will render the same page with error message' do
+    it "on valid, will render the same page with error message" do
       contact = create(:contact, account: user.account)
 
       visit edit_dashboard_contact_path(contact)
 
-      fill_in 'contact[msisdn]', with: ''
-      click_button 'Update Contact'
+      fill_in "contact[msisdn]", with: ""
+      click_button "Update Contact"
 
-      expect(page).to have_text('Edit contact')
+      expect(page).to have_text("Edit contact")
       expect(page).to have_text("can't be blank")
     end
   end

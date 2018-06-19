ccb59d81ef45343216ff15e24200108d83d5b899
Leap Sim <simleap@gmail.com>
2018-06-20 06:48:01 +0700

    add icon to sidebar (#126)
    * add icons to sidebar
    
    * add dashboard spec for icons

--- spec/system/dashboard/dashboard_spec.rb
+++ spec/system/dashboard/dashboard_spec.rb
@@ -14,41 +14,49 @@ RSpec.describe "Dashboard" do
     end
 
     within("#side_navigation") do
+      expect(page).to have_css("i.fa-key")
       expect(page).to have_link(
         I18n.translate!(:"titles.access_tokens.index"),
         href: dashboard_access_tokens_path
       )
 
+      expect(page).to have_css("i.fa-tasks")
       expect(page).to have_link(
         I18n.translate!(:"titles.batch_operations.index"),
         href: dashboard_batch_operations_path
       )
 
+      expect(page).to have_css("i.fa-bullhorn")
       expect(page).to have_link(
         I18n.translate!(:"titles.callouts.index"),
         href: dashboard_callouts_path
       )
 
+      expect(page).to have_css("i.fa-list-ol")
       expect(page).to have_link(
         I18n.translate!(:"titles.callout_participations.index"),
         href: dashboard_callout_participations_path
       )
 
+      expect(page).to have_css("i.fa-address-book")
       expect(page).to have_link(
         I18n.translate!(:"titles.contacts.index"),
         href: dashboard_contacts_path
       )
 
+      expect(page).to have_css("i.fa-info-circle")
       expect(page).to have_link(
         I18n.translate!(:"titles.phone_calls.index"),
         href: dashboard_phone_calls_path
       )
 
+      expect(page).to have_css("i.fa-calendar-alt")
       expect(page).to have_link(
         I18n.translate!(:"titles.remote_phone_call_events.index"),
         href: dashboard_remote_phone_call_events_path
       )
 
+      expect(page).to have_css("i.fa-users")
       expect(page).to have_link(
         I18n.translate!(:"titles.users.index"),
         href: dashboard_users_path

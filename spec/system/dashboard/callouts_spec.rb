5a5dc71e15e64b1f1a041aeedab4071496eb71f2
David Wilkie <dwilkie@gmail.com>
2018-06-23 11:32:46 +0700

    Populate callout when starting it. Remove preview button

--- spec/system/dashboard/callouts_spec.rb
+++ spec/system/dashboard/callouts_spec.rb
@@ -53,8 +53,9 @@ RSpec.describe "Callouts", :aggregate_failures do
     new_callout = Callout.first
     expect(new_callout.audio_file).to be_attached
     expect(new_callout.call_flow_logic).to eq(CallFlowLogic::PlayMessage.to_s)
-    expect(new_callout.callout_population).to be_present
-    expect(new_callout.callout_population.contact_filter_metadata[:commune_id]).to eq(new_callout.commune_ids)
+    callout_population = new_callout.callout_population
+    expect(callout_population).to be_present
+    expect(callout_population.contact_filter_metadata[:commune_id]).to eq(new_callout.commune_ids)
   end
 
   it "can update a callout", :js do
@@ -107,8 +108,6 @@ RSpec.describe "Callouts", :aggregate_failures do
       audio_file: "test.mp3",
       audio_url: "https://example.com/audio.mp3"
     )
-    callout_population = create(:callout_population, callout: callout, account: callout.account)
-
 
     sign_in(user)
     visit dashboard_callout_path(callout)
@@ -116,11 +115,6 @@ RSpec.describe "Callouts", :aggregate_failures do
     expect(page).to have_title("Callout #{callout.id}")
 
     within("#page_actions") do
-      expect(page).to have_link_to_action(
-        :preview,
-        href: dashboard_batch_operation_preview_contacts_path(callout_population)
-      )
-
       expect(page).to have_link_to_action(
         :edit,
         href: edit_dashboard_callout_path(callout)
@@ -158,6 +152,29 @@ RSpec.describe "Callouts", :aggregate_failures do
       expect(page).to have_content("Call flow")
       expect(page).to have_content("Hello World")
     end
+  end
+
+  it "can start a callout" do
+    user = create(:user)
+    callout = create(
+      :callout,
+      :initialized,
+      account: user.account
+    )
+
+    callout_population = create(
+      :callout_population,
+      callout: callout
+    )
+
+    sign_in(user)
+    visit dashboard_callout_path(callout)
+    perform_enqueued_jobs do
+      click_action_button(:start_callout, key: :callouts, type: :link)
+    end
+
+    expect(callout.reload).to be_running
+    expect(callout_population.reload).to be_finished
   end
 
   it "can perform actions on callouts" do

7a22a1ad75bf03b51cb6a821a8a693b27511fe7a
David Wilkie <dwilkie@gmail.com>
2018-08-04 14:16:51 +0700

    Add callout summary

--- app/helpers/dashboard_helper.rb
+++
@@ -1,8 +0,0 @@
-module DashboardHelper
-  def batch_operation_create_phone_calls_default_filter_params
-    default_batch_operation_filter_params = current_account.settings["batch_operation_phone_call_create_parameters"] || {}
-    callout_participation_filter_params = default_batch_operation_filter_params["callout_participation_filter_params"] || {}
-    callout_filter_params = default_batch_operation_filter_params.slice("callout_filter_params")
-    callout_participation_filter_params.merge(callout_filter_params).presence
-  end
-end

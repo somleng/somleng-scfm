709919fb61c79ec80f890b697768419a2ac1d496
David Wilkie <dwilkie@gmail.com>
2018-11-17 15:58:49 +0700

    Improve test coverage

--- app/models/filter/scope/has_phone_calls.rb
+++ app/models/filter/scope/has_phone_calls.rb
@@ -1,6 +1,6 @@
 class Filter::Scope::HasPhoneCalls < Filter::Base
   def apply
-    if has_phone_calls?
+    if ActiveRecord::Type::Boolean.new.cast(params.fetch(:has_phone_calls))
       association_chain.has_phone_calls
     else
       association_chain.no_phone_calls
@@ -8,16 +8,6 @@ class Filter::Scope::HasPhoneCalls < Filter::Base
   end
 
   def apply?
-    has_phone_calls_params.present?
-  end
-
-  private
-
-  def has_phone_calls?
-    ActiveRecord::Type::Boolean.new.cast(has_phone_calls_params)
-  end
-
-  def has_phone_calls_params
-    params[:has_phone_calls]
+    params.key?(:has_phone_calls)
   end
 end

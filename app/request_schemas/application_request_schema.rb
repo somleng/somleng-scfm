d255af17377af2e0f883079a1b3ca996f5063ce1
David Wilkie <dwilkie@gmail.com>
2018-12-09 07:39:22 +0700

    Remove unused schema types

--- app/request_schemas/application_request_schema.rb
+++ app/request_schemas/application_request_schema.rb
@@ -3,14 +3,4 @@ class ApplicationRequestSchema < Dry::Validation::Schema
     config.messages = :i18n
     config.type_specs = true
   end
-
-  module Types
-    include Dry::Types.module
-
-    PhoneNumber = String.constructor do |phone_number|
-      result = PhonyRails.normalize_number(phone_number)
-      result.insert(0, "0") if phone_number.to_s.starts_with?("0")
-      result
-    end
-  end
 end

fcd9c0aecb0244d1adf4da001e5c6e0b3127d65d
David Wilkie <dwilkie@gmail.com>
2018-12-26 11:34:07 +1100

    Improve test coverage

--- app/models/concerns/rails6_backports.rb
+++ app/models/concerns/rails6_backports.rb
@@ -2,12 +2,6 @@ module Rails6Backports
   extend ActiveSupport::Concern
 
   class_methods do
-    def create_or_find_by(attributes, &block)
-      transaction(requires_new: true) { create(attributes, &block) }
-    rescue ActiveRecord::RecordNotUnique
-      find_by!(attributes)
-    end
-
     def create_or_find_by!(attributes, &block)
       transaction(requires_new: true) { create!(attributes, &block) }
     rescue ActiveRecord::RecordNotUnique

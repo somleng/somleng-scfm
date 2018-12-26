0b91c0188f1a1805d6bbb0a12f2899a50bd70b90
David Wilkie <dwilkie@gmail.com>
2018-12-26 11:31:45 +1100

    Improve test coverage

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,10 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   include TimestampQueryHelpers
-
-  if respond_to?(:create_or_find_by)
-    warn("'create_or_find_by' is already defined by Rails!")
-  else
-    include Rails6Backports
-  end
+  include Rails6Backports unless respond_to?(:create_or_find_by)
 end

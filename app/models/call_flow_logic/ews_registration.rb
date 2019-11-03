eeb2075c96f3f5beb6945007320a1ef10c9a11c9
David Wilkie <dwilkie@gmail.com>
2019-11-03 11:38:57 +0700

    WIP

--- app/models/call_flow_logic/ews_registration.rb
+++ app/models/call_flow_logic/ews_registration.rb
@@ -1,112 +1,170 @@
 module CallFlowLogic
   class EWSRegistration < Base
+    INITIAL_STATUS = :answered
+
+    attr_reader :voice_response
+
+    include AASM
+
+    aasm(column: :status, whiny_transitions: false) do
+      state INITIAL_STATUS, initial: true
+      state :playing_introduction
+      state :gathering_province
+      state :gathering_district
+      state :gathering_commune
+      state :playing_conclusion
+      state :finished
+
+      before_all_events :read_status
+      after_all_events :persist_status
+
+      event :process_call do
+        transitions from: :answered,
+                    to: :playing_introduction,
+                    after: :play_introduction
+
+        transitions from: :playing_introduction,
+                    to: :gathering_province,
+                    after: :gather_province
+
+        transitions from: :gathering_province,
+                    to: :gathering_district,
+                    if: :province_gathered?,
+                    after: %i[persist_province gather_district]
+
+        transitions from: :gathering_district,
+                    to: :gathering_commune,
+                    if: :district_gathered?,
+                    after: %i[persist_district gather_commune]
+
+        transitions from: :gathering_commune,
+                    to: :playing_conclusion,
+                    if: :commune_gathered?,
+                    after: %i[persist_commune persist_location]
+
+        transitions from: :playing_conclusion,
+                    to: :finished
+      end
+    end
+
+    def run!
+      transaction do
+        super
+        process_call
+      end
+    end
+
     def to_xml(_options = {})
-      if gathering_data? && dtmf_tones.present?
-        process_input
-      end
-      response = Twilio::TwiML::VoiceResponse.new
-      # response.play(url: "https:/www.example.com/path-to-introduction.mp3")
-      response.say(message: "Welcome to the early warning system registration 1294.")
-      response.gather(action: current_url, action_on_empty_result: true) do |gather|
-        update_phone_call!(status: :gathering_province)
-        gather.say(message: "Please select your province by pressing the corresponding number on your keypad.")
-
-        # gather.play(url: "https://www.example.com/path-to-list-of-provinces.mp3")
-      end
-
-      puts response.to_s
-      response.to_s
-    end
-  end
-
-  private
-
-  def update_phone_call!(data)
-    phone_call.update!(
-      metadata: metadata.deep_merge(data)
-    )
-  end
-
-  def gathering_data?
-    call_status.starts_with?("gathering")
-  end
-
-  def dtmf_tones
-    event.details["Digits"]
-  end
-
-  def pressed_digits
-    dtmf_tones.to_i
-  end
-
-  def process_input
-    return if pressed_digits.zero?
-
-    result = {}
-    if call_status == GATHERING_PROVINCE
-      result[:province] = Selector::LOCATIONS.keys.fetch(dtfm_tones.to_i - 1)
+      voice_response.to_s
     end
 
-    update_phone_call!(result)
-  end
+    private
 
-  def call_status
-    phone_call.metadata["status"].to_s
-  end
+    def read_status
+      aasm.current_state = phone_call.metadata.fetch("status", INITIAL_STATUS).to_sym
+    end
 
-  def phone_call
-    event.phone_call
-  end
+    def persist_status
+      update_phone_call!(status: aasm.to_state)
+    end
 
-  class Selector
-    LOCATIONS = {
-      pursat: {
-        bakan: %i[
-          boeng_bat_kandal
-          boeng_khnar
-          khnar_totueng
-          me_tuek
-          ou_ta_paong
-          rumlech
-          snam_preah
-          svay_doun_kaev
-          ta_lou
-          trapeang_chorng
-        ],
-        kandieng: [],
-        krakor: [],
-        phnum_kravan: [],
-        pursat_municipality: [],
-        veal_veang: []
-      },
-      banteay_meanchey: {},
-      kampong_thom: {},
-      kampot: {},
-      kampong_chhnang: {},
-      siem_reap: {},
-      battambang: {},
-      kratie: {},
-      steung_treng: {},
-      preah_vihear: {},
-      oddar_meanchey: {},
-      kep: {},
-      pailin: {},
-      koh_kong: {},
-      preah_sihanouk: {},
-      kampong_cham: {},
-      tboung_khmum: {},
-      prey_veng: {},
-      ratanakkiri: {},
-      mondulkiri: {},
-      svay_rieng: {},
-      kampong_speu: {}
-    }.freeze
+    def play_introduction
+      @voice_response = Twilio::TwiML::VoiceResponse.new do |response|
+        response.say(message: "Welcome to the early warning system registration 1294.")
+      end
+    end
 
-    attr_reader :phone_call, :digits
+    def gather_province
+      @voice_response = Twilio::TwiML::VoiceResponse.new do |response|
+        response.gather(action_on_empty_result: true) do |gather|
+          gather.say(message: "Please select your province by pressing the corresponding number on your keypad.")
+        end
+      end
+    end
 
-    def initialize(phone_call:, digits:)
-      @phone_call = phone_call
-      @digits = digits
+    def gather_district
+      @voice_response = Twilio::TwiML::VoiceResponse.new do |response|
+        response.gather(action_on_empty_result: true) do |gather|
+          gather.say(message: "Please select your district by pressing the corresponding number on your keypad.")
+        end
+      end
+    end
+
+    def province_gathered?
+      return false if pressed_digits.zero?
+
+      selected_province.present?
+    end
+
+    def selected_province
+      Selector::LOCATIONS.keys[pressed_digits - 1]
+    end
+
+    def persist_province
+      update_phone_call!(province: selected_province)
+    end
+
+    def update_phone_call!(data)
+      phone_call.update!(
+        metadata: phone_call.metadata.deep_merge(data)
+      )
+    end
+
+    def dtmf_tones
+      event.details["Digits"]
+    end
+
+    def pressed_digits
+      dtmf_tones.to_i
+    end
+
+    def phone_call
+      event.phone_call
+    end
+
+    class Selector
+      LOCATIONS = {
+        pursat: {
+          bakan: %i[
+            boeng_bat_kandal
+            boeng_khnar
+            khnar_totueng
+            me_tuek
+            ou_ta_paong
+            rumlech
+            snam_preah
+            svay_doun_kaev
+            ta_lou
+            trapeang_chorng
+          ],
+          kandieng: [],
+          krakor: [],
+          phnum_kravan: [],
+          pursat_municipality: [],
+          veal_veang: []
+        },
+        banteay_meanchey: {},
+        kampong_thom: {},
+        kampot: {},
+        kampong_chhnang: {},
+        siem_reap: {},
+        battambang: {},
+        kratie: {},
+        steung_treng: {},
+        preah_vihear: {},
+        oddar_meanchey: {},
+        kep: {},
+        pailin: {},
+        koh_kong: {},
+        preah_sihanouk: {},
+        kampong_cham: {},
+        tboung_khmum: {},
+        prey_veng: {},
+        ratanakkiri: {},
+        mondulkiri: {},
+        svay_rieng: {},
+        kampong_speu: {}
+      }.freeze
     end
   end
 end
--- spec/models/call_flow_logic/ews_registration_spec.rb
+++ spec/models/call_flow_logic/ews_registration_spec.rb
@@ -1,17 +1,62 @@
 require "rails_helper"
 
 RSpec.describe CallFlowLogic::EWSRegistration do
-  describe "#to_xml" do
-    it "returns TwiML for EWS Registration" do
-      event = create(:remote_phone_call_event)
-      call_flow_logic = CallFlowLogic::EWSRegistration.new(event: event)
+  it "plays an introduction" do
+    event = create_phone_call_event(phone_call_status: nil)
+    call_flow_logic = CallFlowLogic::EWSRegistration.new(event: event)
 
-      xml = call_flow_logic.to_xml
+    call_flow_logic.run!
+    xml = call_flow_logic.to_xml
 
-      response = Hash.from_xml(xml).fetch("Response")
-      expect(response.keys.size).to eq(2)
-      expect(response.fetch("Say")).to eq("Thanks for trying our documentation. Enjoy!")
-      expect(response.fetch("Play")).to eq("http://demo.twilio.com/docs/classic.mp3")
-    end
+    response = parse_response(xml)
+    expect(event.phone_call.metadata.fetch("status")).to eq("playing_introduction")
+    expect(response.keys.size).to eq(1)
+    expect(response.fetch("Say")).to eq("Welcome to the early warning system registration 1294.")
+  end
+
+  it "gathers the province" do
+    event = create_phone_call_event(phone_call_status: :playing_introduction)
+    call_flow_logic = CallFlowLogic::EWSRegistration.new(event: event)
+
+    call_flow_logic.run!
+    xml = call_flow_logic.to_xml
+
+    response = parse_response(xml)
+    expect(event.phone_call.metadata.fetch("status")).to eq("gathering_province")
+    expect(response.keys.size).to eq(1)
+    expect(response.fetch("Gather")).to eq(
+      "actionOnEmptyResult" => "true",
+      "Say" => "Please select your province by pressing the corresponding number on your keypad."
+    )
+  end
+
+  it "gathers the distict" do
+    event = create_phone_call_event(
+      phone_call_status: :gathering_province,
+      event_details: { Digits: "1" }
+    )
+    call_flow_logic = CallFlowLogic::EWSRegistration.new(event: event)
+
+    call_flow_logic.run!
+    xml = call_flow_logic.to_xml
+
+    response = parse_response(xml)
+    expect(event.phone_call.metadata.fetch("status")).to eq("gathering_district")
+    expect(event.phone_call.metadata.fetch("province")).to eq("pursat")
+    expect(response.keys.size).to eq(1)
+    expect(response.fetch("Gather")).to eq(
+      "actionOnEmptyResult" => "true",
+      "Say" => "Please select your district by pressing the corresponding number on your keypad."
+    )
+  end
+
+  def parse_response(xml)
+    Hash.from_xml(xml).fetch("Response")
+  end
+
+  def create_phone_call_event(phone_call_status:, event_details: {})
+    phone_call = create(:phone_call, metadata: { status: phone_call_status }.compact)
+    details = event_details.reverse_merge(attributes_for(:remote_phone_call_event).fetch(:details))
+    create(:remote_phone_call_event, phone_call: phone_call, details: details)
   end
 end

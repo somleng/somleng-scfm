e10dd5c28fa00ca3f24c2e53352e74172c574d7d
David Wilkie <dwilkie@gmail.com>
2019-11-12 11:57:08 +0700

    Fix spec

--- spec/models/call_flow_logic/ews_registration_spec.rb
+++ spec/models/call_flow_logic/ews_registration_spec.rb
@@ -3,15 +3,20 @@ require "rails_helper"
 RSpec.describe CallFlowLogic::EWSRegistration do
   it "plays an introduction" do
     event = create_phone_call_event(phone_call_metadata: { status: nil })
-    call_flow_logic = CallFlowLogic::EWSRegistration.new(event: event)
+    call_flow_logic = CallFlowLogic::EWSRegistration.new(
+      event: event,
+      current_url: "https://scfm.somleng.org/api/remote_phone_call_events"
+    )
 
     call_flow_logic.run!
     xml = call_flow_logic.to_xml
 
     response = parse_response(xml)
     expect(event.phone_call.metadata.fetch("status")).to eq("playing_introduction")
-    expect(response.keys.size).to eq(1)
-    expect(response.fetch("Say")).to eq("Welcome to the early warning system registration 1294.")
+    expect(response).to eq(
+      "Say" => "Welcome to the early warning system registration 1294.",
+      "Redirect" => "https://scfm.somleng.org/api/remote_phone_call_events"
+    )
   end
 
   it "prompts for gathering the province" do
@@ -68,6 +73,45 @@ RSpec.describe CallFlowLogic::EWSRegistration do
       "actionOnEmptyResult" => "true",
       "Say" => "Please select your commune by pressing the corresponding number on your keypad."
     )
+  end
+
+  it "gathers the commune and plays a conclusion" do
+    event = create_phone_call_event(
+      phone_call_metadata: {
+        status: :gathering_commune,
+        province: :pursat,
+        district: :bakan
+      },
+      event_details: { Digits: "5" }
+    )
+    call_flow_logic = CallFlowLogic::EWSRegistration.new(
+      event: event,
+      current_url: "https://scfm.somleng.org/api/remote_phone_call_events"
+    )
+
+    call_flow_logic.run!
+    xml = call_flow_logic.to_xml
+
+    response = parse_response(xml)
+    expect(event.phone_call.metadata.fetch("commune")).to eq("ou_ta_paong")
+    expect(event.phone_call.metadata.fetch("status")).to eq("playing_conclusion")
+    expect(response).to eq(
+      "Say" => "Thank you. You have successfully registered for the EWS System.",
+      "Redirect" => "https://scfm.somleng.org/api/remote_phone_call_events"
+    )
+  end
+
+  it "Hangs up the call" do
+    event = create_phone_call_event(
+      phone_call_metadata: { status: :playing_conclusion }
+    )
+    call_flow_logic = CallFlowLogic::EWSRegistration.new(event: event)
+
+    call_flow_logic.run!
+    xml = call_flow_logic.to_xml
+
+    response = parse_response(xml)
+    expect(response).to have_key("Hangup")
   end
 
   def parse_response(xml)

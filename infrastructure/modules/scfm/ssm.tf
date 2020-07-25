7cbdacacb5d91bc42b9dc83d7bd03f4f731c825b
Samnang Chhun <samnang.chhun@gmail.com>
2020-07-25 16:51:30 +0700

    WIP

--- infrastructure/modules/scfm/ssm.tf
+++ infrastructure/modules/scfm/ssm.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "rails_master_key" {
-  name  = "agent-network.${var.app_environment}.rails_master_key"
+  name  = "scfm.${var.app_environment}.rails_master_key"
   type  = "SecureString"
   value = "change-me"
 

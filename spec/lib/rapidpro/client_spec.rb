b8828baeeb292470359694aac2fb8a1d7091f795
David Wilkie <dwilkie@gmail.com>
2020-08-04 08:48:59 +0700

    WIP

--- spec/lib/rapidpro/client_spec.rb
+++ spec/lib/rapidpro/client_spec.rb
@@ -4,20 +4,23 @@ module Rapidpro
   RSpec.describe Client do
     describe "#start_flow" do
       it "starts a flow" do
-        client = described_class.new(api_token: "api-token")
-        stub_request(:post, "https://app.rapidpro.io/api/v2/flow_starts.json")
+        client = Client.new(api_token: "api-token")
+        stub_request(:post, %r{https://app.rapidpro.io})
 
         client.start_flow(flow: "flow-id", urns: "+25223456789")
 
-        request = WebMock.requests.last
-        expect(request.uri.scheme).to eq("https")
-        expect(request.uri.host).to eq("app.rapidpro.io")
-        expect(request.uri.path).to eq("/api/v2/flow_starts.json")
-        expect(request.headers.fetch("Authorization")).to include("api-token")
-        expect(request.headers.fetch("Content-Type")).to eq("application/json")
-        expect(JSON.parse(request.body)).to include(
-          "flow" => "flow-id",
-          "urns" => "+25223456789"
+        expect(WebMock).to have_requested(
+          :post,
+          "https://app.rapidpro.io/api/v2/flow_starts.json"
+        ).with(
+          headers: {
+            "Authorization" => "Token api-token",
+            "Content-Type" => "application/json"
+          },
+          body: {
+            "flow" => "flow-id",
+            "urns" => "+25223456789"
+          }.to_json
         )
       end
     end

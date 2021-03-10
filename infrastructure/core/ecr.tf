b4d41fb6084f7fbdca1b8dc4039c9a3523f9229d
Samnang Chhun <samnang.chhun@gmail.com>
2021-03-10 17:24:32 +0700

    Switch to AWS ECR public registry

--- infrastructure/core/ecr.tf
+++ infrastructure/core/ecr.tf
@@ -1,15 +1,20 @@
-resource "aws_ecr_repository" "app" {
-  name                 = "scfm"
+resource "aws_ecrpublic_repository" "app" {
+  repository_name = "scfm"
+  provider = aws.us-east-1
 
-  image_scanning_configuration {
-    scan_on_push = true
+  catalog_data {
+    about_text        = "Somleng Simple Call Flow Manager (Somleng SCFM)"
+    architectures     = ["Linux"]
+    description       = "Somleng Simple Call Flow Manager (Somleng SCFM) manages both inbound and outbound calls through Somleng and/or Twilio. Use it to create dynamic, customized and powerful call flows."
   }
 }
 
-resource "aws_ecr_repository" "nginx" {
-  name                 = "scfm-nginx"
+resource "aws_ecrpublic_repository" "nginx" {
+  repository_name = "scfm-nginx"
+  provider = aws.us-east-1
 
-  image_scanning_configuration {
-    scan_on_push = true
+  catalog_data {
+    about_text        = "Somleng SCFM Nginx"
+    architectures     = ["Linux"]
   }
 }

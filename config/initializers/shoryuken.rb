eacdc493f87a53ed7bbd37e6e1a283d66977e440
Samnang Chhun <samnang.chhun@gmail.com>
2021-03-29 14:11:45 +0700

    WIP

--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -1,3 +1,7 @@
+# Reduce the number of requests against SQS
+# https://github.com/ruby-shoryuken/shoryuken/wiki/Shoryuken-options#cache_visibility_timeout
+Shoryuken.cache_visibility_timeout = true
+
 Shoryuken.configure_server do |config|
   config.on(:startup) do
     @server_pid = OkComputer::RackServer.run!
@@ -16,7 +20,6 @@ Shoryuken.configure_server do |config|
   # Turn on long polling
   # https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-short-and-long-polling.html
   # https://github.com/phstc/shoryuken/wiki/Long-Polling
-  config.sqs_client_receive_message_opts = {
-    wait_time_seconds: 20
-  }
+  config.sqs_client_receive_message_opts = { wait_time_seconds: 20 }
+  config.sqs_client_receive_message_opts[:scheduler] = config.sqs_client_receive_message_opts
 end

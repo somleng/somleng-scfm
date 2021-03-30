66e3d976536e23900a5e8aa04f561e64813c9a9c
Samnang Chhun <samnang.chhun@gmail.com>
2021-03-30 17:47:51 +0700

    Don't create another phone call if there is a running already

--- app/jobs/retry_phone_call_job.rb
+++ app/jobs/retry_phone_call_job.rb
@@ -1,8 +1,17 @@
 class RetryPhoneCallJob < ApplicationJob
+  RETRY_CALL_STATUSES = %i[not_answered busy failed].freeze
+  IN_RUNNING_CALL_STATUSES = %i[created queued remotely_queued in_progress].freeze
+
   def perform(phone_call)
+    callout_participation = phone_call.callout_participation
+
+    return if callout_participation.answered?
+    return if callout_participation.phone_calls.where(status: RETRY_CALL_STATUSES).count >= phone_call.account.max_phone_calls_for_callout_participation
+    return if phone_call.callout_participation.phone_calls.where(status: IN_RUNNING_CALL_STATUSES).exists?
+
     PhoneCall.create!(
       account: phone_call.account,
-      callout_participation: phone_call.callout_participation,
+      callout_participation: callout_participation,
       contact: phone_call.contact
     )
   end

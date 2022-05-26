6c7f03eb0c50ff49408638e5c86c459bf742b816
David Wilkie <dwilkie@gmail.com>
2022-05-26 14:19:09 +0700

    Upgrade to RDS serverless V2

--- infrastructure/modules/scfm/rds.tf
+++ infrastructure/modules/scfm/rds.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "db" {
   }
 }
 
-resource "aws_db_subnet_group" "this" {
+resource "aws_db_subnet_group" "db_old" {
   name        = "${var.app_identifier}-db"
   description = "For Aurora cluster somleng SCFM"
   subnet_ids  = var.database_subnets
@@ -34,28 +34,28 @@ resource "aws_ssm_parameter" "db_master_password" {
   }
 }
 
-module "db" {
+module "db_old" {
   source  = "terraform-aws-modules/rds-aurora/aws"
 
   name = var.app_identifier
+  security_group_description = "Managed by Terraform"
   database_name = replace(var.app_identifier, "-", "_")
 
   engine            = "aurora-postgresql"
   engine_mode       = "serverless"
   engine_version    = null
   vpc_id = var.vpc_id
-  db_subnet_group_name = aws_db_subnet_group.this.name
+  create_db_subnet_group = false
+  db_subnet_group_name = aws_db_subnet_group.db_old.name
   allowed_security_groups = [aws_security_group.db.id]
   allowed_cidr_blocks = [var.vpc_cidr_block]
   auto_minor_version_upgrade  = true
   apply_immediately           = true
   storage_encrypted           = true
   monitoring_interval = 60
-  replica_scale_enabled = false
-  replica_count         = 0
 
-  username = var.db_username
-  password = aws_ssm_parameter.db_master_password.value
+  master_username = var.db_username
+  master_password = aws_ssm_parameter.db_master_password.value
   create_random_password = false
   port     = local.database_port
 

9f7d1a8523a7cd48fe44d798295963f8bb4f9f13
David Wilkie <dwilkie@gmail.com>
2022-05-26 16:56:36 +0700

    Remove old database

--- infrastructure/modules/scfm/rds.tf
+++ infrastructure/modules/scfm/rds.tf
@@ -19,16 +19,6 @@ resource "aws_security_group" "db" {
   }
 }
 
-resource "aws_db_subnet_group" "db_old" {
-  name        = "${var.app_identifier}-db"
-  description = "For Aurora cluster somleng SCFM"
-  subnet_ids  = var.database_subnets
-
-  tags = {
-    Name = "aurora-${var.app_identifier}"
-  }
-}
-
 resource "aws_ssm_parameter" "db_master_password" {
   name  = "${var.app_identifier}.db_master_password"
   type  = "SecureString"
@@ -36,40 +26,6 @@ resource "aws_ssm_parameter" "db_master_password" {
 
   lifecycle {
     ignore_changes = [value]
-  }
-}
-
-module "db_old" {
-  source  = "terraform-aws-modules/rds-aurora/aws"
-
-  name = var.app_identifier
-  security_group_description = "Managed by Terraform"
-  database_name = replace(var.app_identifier, "-", "_")
-
-  engine            = "aurora-postgresql"
-  engine_mode       = "serverless"
-  engine_version    = null
-  vpc_id = var.vpc_id
-  create_db_subnet_group = false
-  db_subnet_group_name = aws_db_subnet_group.db_old.name
-  allowed_security_groups = [aws_security_group.db.id]
-  allowed_cidr_blocks = [var.vpc_cidr_block]
-  auto_minor_version_upgrade  = true
-  apply_immediately           = true
-  storage_encrypted           = true
-  monitoring_interval = 60
-
-  master_username = var.db_username
-  master_password = aws_ssm_parameter.db_master_password.value
-  create_random_password = false
-  port     = local.database_port
-
-  scaling_configuration = {
-    auto_pause               = true
-    min_capacity             = 2
-    max_capacity             = 64
-    seconds_until_auto_pause = 600
-    timeout_action           = "ForceApplyCapacityChange"
   }
 }
 
@@ -96,7 +52,7 @@ resource "aws_rds_cluster" "db" {
   storage_encrypted = true
 
   serverlessv2_scaling_configuration {
-    max_capacity = 2.0
+    max_capacity = 6.0
     min_capacity = 0.5
   }
 }

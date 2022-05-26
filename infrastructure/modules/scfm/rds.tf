4a8f4bce80e66a1a2f4fcad17256e870099b337a
David Wilkie <dwilkie@gmail.com>
2022-05-26 15:39:44 +0700

    Upgrade to RDS serverless V2 (#1221)

--- infrastructure/modules/scfm/rds.tf
+++ infrastructure/modules/scfm/rds.tf
@@ -1,5 +1,6 @@
 locals {
   database_port = 5432
+  identifier = "scfmv2"
 }
 
 resource "aws_security_group" "db" {
@@ -12,9 +13,13 @@ resource "aws_security_group" "db" {
     protocol  = "TCP"
     self      = true
   }
+
+  tags = {
+    Name = "aurora-${local.identifier}"
+  }
 }
 
-resource "aws_db_subnet_group" "this" {
+resource "aws_db_subnet_group" "db_old" {
   name        = "${var.app_identifier}-db"
   description = "For Aurora cluster somleng SCFM"
   subnet_ids  = var.database_subnets
@@ -34,28 +39,28 @@ resource "aws_ssm_parameter" "db_master_password" {
   }
 }
 
-module "db" {
+module "db_old" {
   source  = "terraform-aws-modules/rds-aurora/aws"
 
   name = var.app_identifier
+  security_group_description = "Managed by Terraform"
   database_name = replace(var.app_identifier, "-", "_")
 
   engine            = "aurora-postgresql"
   engine_mode       = "serverless"
   engine_version    = null
   vpc_id = var.vpc_id
-  db_subnet_group_name = aws_db_subnet_group.this.name
+  create_db_subnet_group = false
+  db_subnet_group_name = aws_db_subnet_group.db_old.name
   allowed_security_groups = [aws_security_group.db.id]
   allowed_cidr_blocks = [var.vpc_cidr_block]
   auto_minor_version_upgrade  = true
   apply_immediately           = true
   storage_encrypted           = true
   monitoring_interval = 60
-  replica_scale_enabled = false
-  replica_count         = 0
 
-  username = var.db_username
-  password = aws_ssm_parameter.db_master_password.value
+  master_username = var.db_username
+  master_password = aws_ssm_parameter.db_master_password.value
   create_random_password = false
   port     = local.database_port
 
@@ -66,4 +71,40 @@ module "db" {
     seconds_until_auto_pause = 600
     timeout_action           = "ForceApplyCapacityChange"
   }
+}
+
+resource "aws_db_subnet_group" "db" {
+  name        = local.identifier
+  description = "For Aurora cluster ${local.identifier}"
+  subnet_ids  = var.database_subnets
+
+  tags = {
+    Name = "aurora-${local.identifier}"
+  }
+}
+
+resource "aws_rds_cluster" "db" {
+  cluster_identifier = local.identifier
+  engine             = "aurora-postgresql"
+  engine_mode        = "provisioned"
+  engine_version     = "13.6"
+  master_username    = var.db_username
+  master_password    = aws_ssm_parameter.db_master_password.value
+  vpc_security_group_ids = [aws_security_group.db.id]
+  db_subnet_group_name   = aws_db_subnet_group.db.name
+  skip_final_snapshot = true
+  storage_encrypted = true
+
+  serverlessv2_scaling_configuration {
+    max_capacity = 2.0
+    min_capacity = 0.5
+  }
+}
+
+resource "aws_rds_cluster_instance" "db" {
+  identifier = local.identifier
+  cluster_identifier = aws_rds_cluster.db.id
+  instance_class     = "db.serverless"
+  engine             = aws_rds_cluster.db.engine
+  engine_version     = aws_rds_cluster.db.engine_version
 }

8faf9c4d69423a1b8b1ace2236ab485c3642ad29
David Wilkie <dwilkie@gmail.com>
2023-08-02 17:25:18 +0700

    WIP

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,8 @@
 // Entry point for the build script in your package.json
 import * as bootstrap from "bootstrap"
+
+import jquery from 'jquery'
+window.jQuery = jquery
+window.$ = jquery
+
+require("@nathanvda/cocoon")

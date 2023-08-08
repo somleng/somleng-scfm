39f43209b5a853d9a3833c1841df7a2d8c864d43
David Wilkie <dwilkie@gmail.com>
2023-08-08 07:17:53 +0700

    Fix shoryuken retries

--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -14,11 +14,16 @@ Shoryuken.configure_server do |config|
     end
   end
 
-  config.default_worker_options["auto_visibility_timeout"] = true
-  config.default_worker_options["retry_intervals"] = ->(attempts) { (12.hours.seconds**(attempts / 10.0)).to_i }
+  # `JobWrapper` is defined before initializer
+  ActiveJob::QueueAdapters::ShoryukenAdapter::JobWrapper.shoryuken_options(
+    retry_intervals: ->(attempts) { ExponentialBackoff.new.delay(attempt: attempts) },
+    auto_visibility_timeout: true
+  )
 
   # Turn on long polling
   # https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-short-and-long-polling.html
   # https://github.com/phstc/shoryuken/wiki/Long-Polling
   config.sqs_client_receive_message_opts = { wait_time_seconds: 20 }
 end
+
+

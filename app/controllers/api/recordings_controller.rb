349db774e7bacaf6b475a2a3e099901eb5cd5b3b
David Wilkie <dwilkie@gmail.com>
2024-03-11 14:00:26 +0700

    Add endpoint for downloading recordings (#1464)

--- app/controllers/api/recordings_controller.rb
+++ app/controllers/api/recordings_controller.rb
@@ -1,5 +1,19 @@
 module API
   class RecordingsController < API::BaseController
+    include ActiveStorage::SetCurrent
+    include ActionController::MimeResponds
+
+    respond_to :json, :mp3
+
+    def show
+      recording = find_resource
+
+      respond_to do |format|
+        format.json { respond_with_resource }
+        format.mp3 { redirect_to(recording.audio_file.url, allow_other_host: true) }
+      end
+    end
+
     private
 
     def association_chain
